15eb84b34bbcda11ae2544ef333f937d272b4156
Your Name <you@example.com>
2020-07-18 16:41:18 +0530

    fuctionality of check correctness

--- RDIP/Codes/sentence.js
+++ RDIP/Codes/sentence.js
@@ -1,5 +1,9 @@
 var button_cnt;
 var hin_btn;
+var x,y;
+var array1;
+var array2;
+
 function array() {
 var e = document.getElementById("ar");
 var strUser = e.options[e.selectedIndex].text;
@@ -13,8 +17,8 @@ document.getElementById('b').innerHTML="";
 document.getElementById('i').innerHTML="";
 document.getElementById('c').style.display ="none";
   document.getElementById('d').style.display ="none"; 
-  hin_btn=0;
-  button_cnt=0;
+  document.getElementById("right").style.display="none";
+  document.getElementById("wrong").style.display="none";
   hin2=0;
   word_cnt=0;
   sentence="";
@@ -26,6 +30,17 @@ document.getElementById(k).innerHTML="";
 document.getElementById(k2).innerHTML="";
 }
 if(strUser=="English"){
+  array1=[["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
+["some students like to study in the night","at night some students like to study"],
+["John and Mary went to church","Mary and John went to church"],
+["John went to church after eating","after eating John went to church","John after eating went to church"],
+["did he go to market","he did go to market"],
+["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
+["John goes to the library and studies","John studies and goes to the library"],
+["John ate an apple so did she","she ate an apple so did John"],
+["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
+["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]];
+
 document.getElementById("h2h").innerHTML="";
 document.getElementById('reform').style.display = '';
 document.getElementById('reform').innerHTML="";
@@ -34,20 +49,10 @@ var p=i+'h';
 var p2=i+'';
 document.getElementById(p).style.display = 'none';
 document.getElementById(p2).style.display = '';
+//document.getElementById(p2).innerHTML="";
 }
-
-array[1]=["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"];
-array[2]=["some students like to study in the night","at night some students like to study"];
-array[3]=["John and Mary went to church","Mary and John went to church"];
-array[4]=["John went to church after eating","after eating John went to church","John after eating went to church"];
-array[5]=["did he go to market","he did go to market"];
-array[6]=["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"]
-array[7]=["John goes to the library and studies","John studies and goes to the library"];
-array[8]=["John ate an apple so did she","she ate an apple so did John"];
-array[9]=["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"];
-array[10]=["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"];
-var x = Math.floor((Math.random() * 10) + 1);
-var res=shuffle(array[x][0]);
+x = Math.floor(Math.random() * 10);
+var res=shuffle(array1[x][0]);
 
 var words = res.split(" ");
 var l=words.length;
@@ -56,6 +61,7 @@ words[i] += " ";
 }
 
 for( var i=0;i<l;i++){
+  //alert(x);
 var butid=i+'b'
 var element = document.createElement("input");
 element.setAttribute("type", 'button');
@@ -63,10 +69,19 @@ element.setAttribute("value", words[i]);
 element.setAttribute('id',butid)
 var foo = document.getElementById(i+'');
 foo.appendChild(element);
+//alert(words[i]);
 }  
 button_cnt=l;
 }
 else if(strUser=="Hindi"){
+  array2=[["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
+["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
+["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],
+["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"],
+["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
+["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
+["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
+
 document.getElementById("h2id").innerHTML="";
  document.getElementById('ref').style.display = '';
  document.getElementById("ref").innerHTML="";
@@ -75,23 +90,19 @@ for( var i=0;i<=9;i++){
   var r2=i+'h';
   document.getElementById(r).style.display = 'none';
   document.getElementById(r2).style.display = '';
+  //document.getElementById(r2).innerHTML="";
 }
+ y = Math.floor(Math.random() * 7);
 
-array[1]=["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"];
-array[2]=["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"];
-array[3]=["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"];
-array[4]=["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"];
-array[5]=["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"];
-array[6]=["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"];
-array[7]=["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"];
-var y = Math.floor((Math.random() * 7) + 1);
-var res=shuffle(array[y][0]);
+var res=shuffle(array2[y][0]);
 var words = res.split(" ");
-var l=words.length;
-for (var i = 0; i <l; i++) {
+var l1=words.length;
+for (var i = 0; i <l1; i++) {
 words[i] += " ";
 }
-for( var i=0;i<l;i++){
+
+for( var i=0;i<l1;i++){
+  // alert(y);
 var butid=i+'hx'
 var element = document.createElement("input");
 element.setAttribute("type", 'button');
@@ -99,8 +110,9 @@ element.setAttribute("value", words[i]);
 element.setAttribute('id',butid)
 var foo = document.getElementById(i+'h');
 foo.appendChild(element);
+//alert(words[i]);
 }
-hin_btn=l;
+hin_btn=l1
 
 }
 }
@@ -122,7 +134,7 @@ var word_cnt=0;
 var sentence="";
 function sec(clicked){
   document.getElementById('c').innerHTML ="";
-document.getElementById('d').innerHTML ="";
+  document.getElementById('d').innerHTML ="";
   document.getElementById('d').style.display ="none";
   document.getElementById('c').style.display ="none";
   document.getElementById('reform').style.display = '';
@@ -159,9 +171,7 @@ element.setAttribute("value", "Check the correctness of this sentence");
 element.setAttribute("id", "correct");
 var foo = document.getElementById("c");
 foo.appendChild(element);
-
 word_cnt=0;
-sentence="";
 }
 }
 var hin_sentence="";
@@ -205,16 +215,14 @@ element.setAttribute("value", "Check the correctness of this sentence");
 element.setAttribute("id", "correct_hin");
 var foo = document.getElementById("d");
 foo.appendChild(element);
-
 hin2=0;
-hin_sentence="";
 }
 }
 
 function reform_hin_sentence()
 {
 document.getElementById('h2h').innerHTML=''
-for(var i=0;i<9;i++){
+for(var i=0;i<=9;i++){
 var clickid=i+"hx"
 document.getElementById('b').innerHTML="Formed Sentence";
 document.getElementById('i').innerHTML=" (after selecting words):";
@@ -225,13 +233,16 @@ document.getElementById('b').style.display = "none";
 document.getElementById('ref').style.display = "none";
 document.getElementById('d').style.display="none";
 document.getElementById('c').style.display="none";
+document.getElementById('wrong').style.display="none";
+hin2=0;
+hin_sentence="";
 }
 
 }
 function reform_sentence()
 {
 document.getElementById('h2id').innerHTML=''
-for(var i=0;i<9;i++){
+for(var i=0;i<=9;i++){
 var clickid=i+"b"
 document.getElementById('b').innerHTML="Formed Sentence";
 document.getElementById('i').innerHTML=" (after selecting words):";
@@ -242,7 +253,49 @@ document.getElementById('b').style.display = "none";
 document.getElementById('reform').style.display = "none";
 document.getElementById('c').style.display="none";
 document.getElementById('d').style.display="none";
+document.getElementById('right').style.display="none";
+word_cnt=0;
+sentence="";
 
 }
+
+}
+function check_correctness() {
+  document.getElementById("right").innerHTML="";
+  document.getElementById("right").style.display='';
+  document.getElementById("wrong").innerHTML="";
+  document.getElementById("wrong").style.display='';
+  var txt=sentence.trim();
+  var q=array1[x].length;
+  var m=0;
+  for(var i=0;i<q;i++){
+    str=array1[x][i];
+  
+    var m=str.localeCompare(txt);
+    if(m==0){
+      document.getElementById("right").innerHTML="Right answer!!!";
+      return ;
+    }
+    
+ }
+  document.getElementById("wrong").innerHTML="Wrong answer!!!";
+}
+function hin_correctness(){
+  document.getElementById("wrong").innerHTML="";
+  document.getElementById("wrong").style.display='';
+  document.getElementById("right").innerHTML="";
+  document.getElementById("right").style.display='';
+  var txt=hin_sentence.trim(); 
+  var q=array2[y].length;
+  var m=0;
+  for(var i=0;i<q;i++){
+    str=array2[y][i];  
+    var m=str.localeCompare(txt);
+    if(m==0){
+      document.getElementById("right").innerHTML="Right answer!!!";
+      return ;
+    } 
+ }
+  document.getElementById("wrong").innerHTML="Wrong answer!!!";
 
 }
